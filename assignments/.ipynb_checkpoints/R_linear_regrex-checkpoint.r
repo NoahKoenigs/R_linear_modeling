04c428a526e8e9f0308ec6fff62812a42d9362bf
Noah Koenigs <koenigs.5@buckeyemail.osu.edu>
2023-07-14 22:55:18 +0000

    added png files

--- assignments/.ipynb_checkpoints/R_linear_regrex-checkpoint.r
+++ assignments/.ipynb_checkpoints/R_linear_regrex-checkpoint.r
@@ -1,26 +1,34 @@
-#Package Install
+#!/usr/env/binbash/assignments
+
+## Package Install
 install.packages ("ggplot2")
 
-#Import dataset
+## Importong CSV file
 dataset = read.csv ("regrex1.csv")
 print(dataset)
-model = lm(formula = x ~ y, data = dataset)
+model = lm(formula = y ~ x, data = dataset)
 
 #Scatterplot
 library (ggplot2) 
 ggplot() +
     geom_point(aes(x = dataset$ x, y = dataset$ y),color = 'orange')
+ggsave(filename = "scatterplot.png", plot = linegraph)
 
-#Line Graph
+#Lineplot
+### does not print png!
 library (ggplot2) 
 ggplot() +
 geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
                 color = 'green')
-#Linear Model
+
+# Regression model with summary
 library(ggplot2) 
 ggplot() +
   geom_point(aes(x = dataset$x, y = dataset$y), color = 'orange') +
   geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)), color = 'green') +
   ggtitle("x vs y")
+ggsave(filename = "Regression.png", plot = linegraph)
+summary(model)
+
 
 
