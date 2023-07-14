4e141b1974af698ef3a13b796fde790ada431255
Noah Koenigs <koenigs.5@buckeyemail.osu.edu>
2023-07-14 04:28:04 +0000

    made executable-ish script

---
+++ assignments/.ipynb_checkpoints/R_linear_regrex-checkpoint.r
@@ -0,0 +1,26 @@
+#Package Install
+install.packages ("ggplot2")
+
+#Import dataset
+dataset = read.csv ("regrex1.csv")
+print(dataset)
+model = lm(formula = x ~ y, data = dataset)
+
+#Scatterplot
+library (ggplot2) 
+ggplot() +
+    geom_point(aes(x = dataset$ x, y = dataset$ y),color = 'orange')
+
+#Line Graph
+library (ggplot2) 
+ggplot() +
+geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)),
+                color = 'green')
+#Linear Model
+library(ggplot2) 
+ggplot() +
+  geom_point(aes(x = dataset$x, y = dataset$y), color = 'orange') +
+  geom_line(aes(x = dataset$x, y = predict(model, newdata = dataset)), color = 'green') +
+  ggtitle("x vs y")
+
+
